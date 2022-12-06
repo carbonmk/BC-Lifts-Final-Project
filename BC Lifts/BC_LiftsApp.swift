9ece9ad128aac57691b1239cc9f3e84df8195a01
Michael Carbone <carbonmk@bc.edu>
2022-12-06 14:57:43 -0500

    Initial Commit

---
+++ BC Lifts/BC_LiftsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BC_LiftsApp.swift
+//  BC Lifts
+//
+//  Created by Michael Carbone on 12/6/22.
+//
+
+import SwiftUI
+
+@main
+struct BC_LiftsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BC Lifts/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  BC Lifts
+//
+//  Created by Michael Carbone on 12/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
